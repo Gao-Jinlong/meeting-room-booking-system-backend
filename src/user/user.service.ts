8e018f5406b9e75b33085e0da18cc0c62075d014
gao-jinlong <2675130594@qq.com>
2023-11-25 00:04:37 +0800

    feat(register)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,58 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { RedisService } from 'src/redis/redis.service';
+import { md5 } from 'src/utils';
 
 @Injectable()
 export class UserService {
+  @Inject(RedisService)
+  private redisService: RedisService;
   private logger = new Logger();
 
   @InjectRepository(User)
   private userRepository: Repository<User>;
-  async register(register: RegisterUserDto) {}
+  async register(register: RegisterUserDto) {
+    const captcha = await this.redisService.get(`captcha_${register.email}`);
+
+    if (!captcha) {
+      throw new HttpException('验证码已过期', HttpStatus.BAD_REQUEST);
+    }
+
+    if (captcha !== register.captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOneBy({
+      username: register.username,
+    });
+
+    if (foundUser) {
+      throw new HttpException('用户名已存在', HttpStatus.BAD_REQUEST);
+    }
+
+    const newUser = new User();
+    newUser.username = register.username;
+    newUser.password = md5(register.password);
+    newUser.nickName = register.nickName;
+    newUser.email = register.email;
+
+    try {
+      await this.userRepository.save(newUser);
+      return {
+        success: true,
+      };
+    } catch (e) {
+      this.logger.error(e, UserService);
+      throw new HttpException('注册失败', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
